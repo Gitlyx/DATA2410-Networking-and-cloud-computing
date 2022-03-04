424a4b5104c1508cc19f50c73ea08e08ad3fe536
Charlie <charlievo22@gmail.com>
2022-03-04 22:27:48 +0100

    Created randomResponse Fn

--- bot.py
+++ bot.py
@@ -2,5 +2,11 @@ import responses as resp
 
 
 def getResponse(action):
-    response
-    return response
+    family = ['Cave dad', 'Cave mom', 'Cave baby']
+
+    for i in family:
+        print(i + ": " + resp.randomResponse(action))
+
+
+while True:
+    getResponse(input("\nDo you guys wanna: "))
--- responses.py
+++ responses.py
@@ -1,12 +1,12 @@
 import random
 
 
-def profoundResponse(noun, noun2=None):
+def profoundResponse(noun):
     alt1 = ['ARRGHH! ', 'Hmpf.. ', '... ', 'Grr.. ']
     return (random.choice(alt1) + "Me no like {}".format(noun+'ing!'))
 
 
-def smartResponse(noun, noun2=None):
+def smartResponse(noun):
     alt1 = ['{}?, let me see .. ',
             '{} is a bit .. ',
             '{}ing you say? ',
@@ -15,6 +15,12 @@ def smartResponse(noun, noun2=None):
     return(random.choice(alt1).format(noun) + 'I can do {}'.format(noun+'ing.'))
 
 
-def lazyResponse(noun, noun2=['sleep', 'rest', 'eat']):
+def lazyResponse(noun):
     alt1 = ['weak', 'slow', 'tired', 'hungry']
-    return('I am too {} to {}, '.format(random.choice(alt1), noun) + 'I rather go {}.'.format(random.choice(noun2)))
+    alt2=['sleep', 'rest', 'eat']
+    return('I am too {} to {}, '.format(random.choice(alt1), noun) + 'I rather go {}.'.format(random.choice(alt2)))
+
+
+def randomResponse(noun):
+    responses = [profoundResponse, smartResponse, lazyResponse]
+    return random.choice(responses)(noun)
