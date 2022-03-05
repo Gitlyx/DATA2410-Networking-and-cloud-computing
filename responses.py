bd60f689775e13eed69fa0464d5921afd46fe021
Charlie <charlievo22@gmail.com>
2022-03-05 21:40:41 +0100

    Updated chat lines

--- responses.py
+++ responses.py
@@ -1,9 +1,10 @@
 import random
 
 people = ['Ulrik', 'Johan', 'Mohammed', 'Ahsan', 'Charlie', 'Adrian']
-triggerGreeintg = ['hello', 'hey', 'sup', 'hi', 'hei', 'skjera?']
+triggerGreeting = ['hello', 'hey', 'sup', 'hi', 'hei', 'skjera']
 triggerFarewell = ['bye', 'goodbye', 'farewell', 'cya']
-triggerHelp = ['--help', '-h', 'help']
+triggerExit = ['exit']
+triggerHelp = ['help', 'h', '--help', '-h']
 
 
 def replyGreeting():
@@ -18,31 +19,36 @@ def replyGreeting():
 
 def replyFarewell():
     farewell = [
-        'Oh nei ikke stikk',
-        'Snakkes bro',
-        'HEY! Du skylder meg en kebab!',
-        'Brur kom hvor skal du?!',
-        'Ææælig, kom her',
-        'Venta brur',
+        'Oh you\'re leaving already?',
+        'Dont leave without me!',
+        'Cya next friday!',
+        'Sayonara my guy',
+        'Good bye!',
     ]
     response = random.choice(farewell)
     return response
 
 
+def replyExit():
+    for person in people:
+        print(person + ": " + replyFarewell())
+    return "*waves*"
+
+
+def replyHelp():
+    alt1 = '/'.join(triggerGreeting)
+    alt2 = '/'.join(triggerFarewell)
+
+    return ('To be able to communicate with us, you can try one of the following: [' + alt1 + '] or [' + alt2 + '].')
+
+
+def unknownReply():
+    unknown = [
+        'Not sure what you mean ...',
+        'Please, I\'m not the right guy for this',
+        'I don\'t think I understand.'
+    ]
+    return random.choice(unknown)
+
+
 def oneReply(): return random.choice(people)
-
-
-def manyReplies():
-    randomPeople = people.copy()
-    random.shuffle(randomPeople)
-    return randomPeople[:random.randrange(len(people))+1]
-
-
-def allReplies(): return people
-
-
-def allGoodbye():
-    byeAll = allReplies()
-    for i in byeAll:
-        print(i, ": \t", replyFarewell())
-    print("\n")
