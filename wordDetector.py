4eee13a33b161d03405cf82f5c78b1400e7b96fc
Charlie <charlievo22@gmail.com>
2022-03-05 14:54:38 +0100

    Introduced word detection algorithm

---
+++ wordDetector.py
@@ -0,0 +1,36 @@
+import re
+
+# Logic for detecting words in a sentence. This allows the bots to respond without having an exact word count.
+
+
+def messageScanner(userMessage, wordPool, singleResponse=False, requiredWords=[]):
+    wordsDetected = 0
+    hasRequiredWords = True
+
+    #Word counter for detected words in the entire sentence.
+    for word in userMessage:
+        if word in wordPool:
+            wordsDetected += 1
+    #Calculates the match rate compared to the words detected.
+    matchRate = float(wordsDetected) / float(requiredWords)
+
+    #Checks if any of the words are in the required word list.
+    for word in requiredWords:
+        if word not in userMessage:
+            hasRequiredWords = False
+            break
+
+    #Return function triggers only if the sentence has required words or a single word.
+    if hasRequiredWords or singleResponse:
+        return int(matchRate * 100)
+    else:
+        return 0
+
+
+def responseTrigger(userMessage):
+    matchRateList = []
+
+    def reply(response, wordPool, singleResponse=False, requiredWords=[]):
+        nonlocal matchRateList
+        matchRateList[response] = messageScanner(
+            userMessage, wordPool, singleResponse, requiredWords)
