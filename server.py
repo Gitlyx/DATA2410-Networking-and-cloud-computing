9844f1519c7d5f3641869a2d0e3e263d6fff0a5f
Charlie <charlievo22@gmail.com>
2022-03-08 19:33:55 +0100

    Reconstructed listener

--- server.py
+++ server.py
@@ -6,23 +6,16 @@ client_list = []
 
 
 def connect_client(client_socket, client_address):
-
     client_list.append(client_socket)
     client_socket.send('Connection successful.'.encode('utf8'))
     server_broadcast(f'IP: {client_address} has conncted to the server.')
-
-
-def server_listener():
-    while True:
-        message = client_socket.recv(1024).decode('utf8')
-        server_broadcast(message)
 
 
 def server_broadcast(message):
 
     for client in client_list:
         message = f'{client_socket}: {message}'
-        client.send(message.encode('utf8'))
+        client.send(message.encode())
 
     server_commands(client_socket, message)
 
@@ -38,11 +31,15 @@ def server_commands(client_socket, message):
 # Socket implementation
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((socket.gethostname(), 2345))
-server_socket.listen(5)
+server_socket.listen(4)
 print("Listening to connections ...")
 
 while True:
     client_socket, client_address = server_socket.accept()
-    print(f'{client_address} has connected ')
     connect_client(client_socket, client_address)
-    server_listener()
+
+    message = client_socket.recv(1024).decode()
+    print(f'{client_address} has connected ')
+
+    server_broadcast(message)
+    print(f'{message}')
