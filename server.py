0c2ca8a73fb9ddd009e0ac2f261acad95091d0f0
Charlie <charlievo22@gmail.com>
2022-03-09 19:39:20 +0100

    Made room for threading changes

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from _thread import *
 
 # ----------------------------------------------------------------
 # Client variabels, functions
@@ -7,28 +8,19 @@ client_list = []
 
 def connect_client(client_socket, client_address):
     client_list.append(client_socket)
-    client_socket.send('Connection successful.'.encode('utf8'))
-    server_broadcast(f'IP: {client_address} has conncted to the server.')
+    client_socket.send('Connection successful.'.encode())
+    broadcast(f'IP: {client_address} has conncted to the server.')
 
 
-def server_broadcast(message):
+def broadcast(message):
 
     for client in client_list:
         message = f'{client_socket}: {message}'
         client.send(message.encode())
 
-    server_commands(client_socket, message)
-
-
-def server_commands(client_socket, message):
-    if message == 'exit':
-        server_broadcast(f'{client_socket} has left.')
-        client_socket.send("Closing the server ...".encode())
-        client_socket.close()
-
 
 # ----------------------------------------------------------------
-# Socket implementation
+# Server socket implementation
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((socket.gethostname(), 2345))
 server_socket.listen(4)
@@ -37,9 +29,7 @@ print("Listening to connections ...")
 while True:
     client_socket, client_address = server_socket.accept()
     connect_client(client_socket, client_address)
-
-    message = client_socket.recv(1024).decode()
     print(f'{client_address} has connected ')
 
-    server_broadcast(message)
-    print(f'{message}')
+
+
