c2cc824436c20b32443d3f3c2610063e683fb9e0
Charlie Vo <charlievo22@gmail.com>
2022-03-11 14:45:01 +0100

    Implemented commandline arguments

--- client.py
+++ client.py
@@ -1,33 +1,35 @@
 import socket
+import sys
 import threading
 from time import sleep
 
-IP = socket.gethostname()
-PORT = 2345
-BOT = None
+# IP = sys.argv[1]
+# PORT = int(sys.argv[2])
+# BOT = sys.argv[3]
+
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
 
 print(IP)
 print(PORT)
 print(BOT)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
+def activate_bot():
+    botlogic = ''
 
 
-def initiate_bot():
-    print('Bot ')
-
-
-if BOT:
-    initiate_bot()
-
-
-def send():
+def activate_user():
     while True:
         sleep(1)
         message = input("You: ")
         client_socket.send(("You: " + message).encode())
 
+
+if BOT:
+    threading.Thread(target=activate_bot).start()
+else:
+    threading.Thread(target=activate_user).start()
 
 def recieve():
     while True:
@@ -36,4 +38,3 @@ def recieve():
 
 
 threading.Thread(target=recieve).start()
-threading.Thread(target=send).start()
