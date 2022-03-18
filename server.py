335314f8319e4d73348ca6fae3a7032e94bc2e26
Charlie Vo <charlievo22@gmail.com>
2022-03-18 23:27:47 +0100

    Added comments to server file

--- server.py
+++ server.py
@@ -17,20 +17,18 @@ def start_server():
         client_socket, client_address = server_socket.accept()
         client_list.append(client_socket)
 
-        #client.py automatically sends its username upon connection.
+        # client.py automatically sends its username upon connection.
         username = client_socket.recv(1024).decode()
         username_list.append(username)
 
         broadcast_message(f"{username} has entered the chat.")
         print(f"{username} has entered the chat.")
 
-        #Server will now have its own thead listening to the connected client.
+        # Each connection has its own thread listening to responses.
         threading.Thread(target=server_recv, args=(client_socket,)).start()
-    
 
-
-# Listener with an exception handler that closes the socket and ends the thread upon error detection.
-# This prevents the server from idling due to BrokenPipeErrors that occurs upon read/write error on sockets.
+# The server_recv is listening to any responses the client is sending. 
+# The try/catch- block is implemented to close the connection upon errors such as infinite loops and BrokenPipe errors.
 def server_recv(client_socket):
     while True:
         try:
@@ -42,34 +40,36 @@ def server_recv(client_socket):
             break
 
 
-# This code block is functions that lets the server handle singular tasks.
-# Functions are made to simplify the code and to easier understand what actinos the server is doing.
+# I have broken down all frequently used tasks into functions for easier readability.
+# These functions are made to more easily understand which tasks the server has to handle.
 def broadcast_message(message):
+    # Breoadcasts all the messages accross all connections.
     for client in client_list:
         client.sendall(message.encode())
 
 def send_message(client_socket, message):
-    # Find the username of the sending client.
+    # Sends our messages to other clients using "{username}: {message}" formatting.
     username = get_username(client_socket)
     message = f'{username}: {message}'
     for client in client_list:
         if client != client_socket and client_socket != "":
             client.sendall(message.encode())
-    
     print(f'{message}', end='\n')
 
-
 def get_username(client_socket):
+    # Looks up which username each client has based on the matching connection data.
     index = client_list.index(client_socket)
     username = username_list[index]
     return username
 
 def get_client_socket(username):
+    # Looks up what connection data each user has based on their username.
     index = username_list.index(username)
     client_socket = client_list[index]
     return client_socket
 
 def close_connection(client_socket, username):
+    # Closes a single connection based on parameter: username. 
     if username in username_list:
         client_connection = get_client_socket(username)
         client_connection.sendall(f'{username} has been disconnected.'.encode())
@@ -81,9 +81,8 @@ def close_connection(client_socket, username):
         response = (f'Could not find {username} in list of connections.')
         client_socket.sendall(response.encode())
         
-        
-
 def close_all_connections(client_socket):
+    # Closes up all connections, allowing the user to be the last connection closed.
     for client in client_list:
         try:
             if client != client_socket:
@@ -98,15 +97,16 @@ def close_all_connections(client_socket):
 
 
 
-# This code block handles the commands sent from the user if command criterias are met.
-# The message is then parsed and checks if the server can handle any of the requests.
+# This code block is for handling commands sent from the user with "--" prefix.
+# All the commands are made server side so that the server is able to handle all the clients indevidually.
 def command(client_socket,message):
-    if len(message.split()) <= 2 and message.find('-') == 0:
+    # Checks if the message is a command by confirming that it is limited to max 2 substring AND as well as including the prefix "--".
+    if len(message.split()) <= 2 and message.find('--') == 0:
         execute_commands(client_socket, message)
 
 
 def execute_commands(client_socket, message):
-    
+    # Each command has to be an exact match, otherwise the user is promted to use one of the existing commands available.    
     if '--help' in message:
         response = 'Try communicating with the connected clients by suggesting an action such as "Anyone want to play a game?"'
         print(response)
@@ -122,7 +122,7 @@ def execute_commands(client_socket, message):
         close_connection(client_socket, username)
 
     else:
-        message = 'Unknown command. Try [--help/--exit].'
+        message = 'Unknown command. Try [--help/--exit/--kick \{username\}].'
         client_socket.sendall(message.encode())
 
 start_server()
